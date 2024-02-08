99e81193e536dc11ee27ea487f7305de678448d4
Timothé <boyer.tim.travail@gmail.com>
2024-02-08 17:22:58 +0100

    fin

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from firstDescent import*
+from momentumDescent import*
 
 def f(X):
     return X[0]**2+X[1]**2
 
 i=np.array([16.0,16.0])
 
-print(firstDescent(f,i,0.1,0.1,0.5))
+print(momentumGradientDescent(f,i,0.1,0.1,1))
