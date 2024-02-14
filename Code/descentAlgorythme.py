994d1b4d61d31982d67785c728bfb94c4753a5b7
Timothé <boyer.tim.travail@gmail.com>
2024-02-14 10:56:12 +0100

    Nestrov and momentum method

--- Code/descentAlgorythme.py
+++ Code/descentAlgorythme.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from gradientFunction import*
 
 def firstDescent(function: function, evaluationPoint: np.array, stepDerivative:float, stepDescent:float, terminationCondition:float)->list[tuple[np.array, np.array]]:
@@ -27,13 +28,13 @@ def descentVarientStep(function:function, evaluationPoint:np.array, stepDerivati
 
     Args:
         function (function): function from which we want to descend. The function must take an array in parameter even if it's à function with one variable.
-        evaluationPoint (array): Initial Assessment Point
+        evaluationPoint (np.array): Initial Assessment Point
         stepDerivative (float): Deviation used to derive
-        stepDescent (float): Steps of the descent
+        stepDescent (function): Function calculating the kth step of descent
         terminationCondition (float): Value of the maximum gradient norm to stop the descent
         
     Returns:
-        list[tupple(array,array)]: List of evaluation points through which the descent has passed and their assiociate gradient
+        list[tupple[np.array,np.array]]: List of evaluation points through which the descent has passed and their assiociate gradient
     """
     path=[]
     counterIteration=0
@@ -45,4 +46,58 @@ def descentVarientStep(function:function, evaluationPoint:np.array, stepDerivati
         path.append((evaluationPoint.copy(),gradient))
         counterIteration+=1
     return path
+
+def heavyBallDescent(function:function, evaluationPoint:np.array, stepDerivative:float, stepDescent:float, terminaisonCondition:float, stepMomentum: float)->list[tuple[np.array, np.array]]:
+    """Perform the heavy ball gradient descent on the specified function
+
+    Args:
+        function (function): function from which we want to descend. The function must take an array in parameter even if it's à function with one variable.
+        evaluationPoint (np.array): Initial Assessment Point
+        stepDerivative (float): Deviation used to derive
+        stepDescent (float): Steps of the descent
+        terminationCondition (float): Value of the maximum momentum norm to stop the descent
+        stepMomentum (float): A value between 0 and 1 indicating the importance of the previous term for the recalculation of momentum
+
+    Returns:
+        list[tupple[np.array,np.array]]: List of evaluation points through which the descent has passed and their assiociate gradient
+    """
+    path=[]
+    momentum=gradientFunction(function, evaluationPoint, stepDerivative)
+    path.append((evaluationPoint.copy(), momentum.copy()))
+    while np.linalg.norm(gradientFunction(function, evaluationPoint, stepDerivative)) >= terminaisonCondition:
+        evaluationPoint-=stepDescent*momentum
+        momentum=stepMomentum*momentum+(1-stepMomentum)*gradientFunction(function, evaluationPoint, stepDerivative)
+        path.append((evaluationPoint.copy(), momentum.copy()))
+    return path
+
+def nesterovDescent(function: function, evaluationPoint: np.array, stepDerivative: float, stepDescent:float, terminaisonCondition:float)->list[tuple[np.array, np.array]]:
+    """Perform the heavy ball gradient descent on the specified function
+
+    Args:
+        function (function): function from which we want to descend. The function must take an array in parameter even if it's à function with one variable.
+        evaluationPoint (np.array): Initial Assessment Point
+        stepDerivative (float): Deviation used to derive
+        stepDescent (float): Steps of the descent
+        terminationCondition (float): Value of the maximum momentum norm to stop the descent
+
+    Returns:
+        list[tupple[np.array,np.array]]: List of evaluation points through which the descent has passed and their assiociate gradient
+    """
+    path=[]
+    lambdaPrevius=0
+    lambdaCurrent=1
+    beta=1
+    path.append(evaluationPoint.copy())
+    path.append(evaluationPoint.copy())
+    while np.linalg.norm(gradientFunction(function, evaluationPoint, stepDerivative)) >= terminaisonCondition:
+        evaluationPoint=evaluationPoint-stepDerivative*gradientFunction(function, path[-1]+beta*(path[-1]-path[-2]), stepDerivative)+beta*(path[-1]-path[-2])
+        
+        lambdaTemp=lambdaCurrent
+        lambdaCurrent=(1+sqrt(1+4*lambdaCurrent**2))/2
+        lambdaPrevius=lambdaTemp
+        
+        beta=(lambdaPrevius-1)/lambdaCurrent
+        
+        path.append(evaluationPoint)
+    return path[1::]
 
--- Code/main.py
+++ Code/main.py
@@ -5,6 +5,11 @@ def f1(X):
     return X[0]**2
 
 def f2(X):
-    return X[0]+X[1]**2
+    return X[0]**2+X[1]**2+X[0]*X[1]
 
-print(firstDescent(f1, np.array([16.0]), 0.1, 0.1, 1))
+f1= lambda X: X[0]**2
+f2=lambda X: X[0]**2+X[1]**2+X[0]*X[1]
+
+print(len(firstDescent(f1, np.array([14774.0]), 0.1, 0.1, 0.001)))
+print(len(heavyBallDescent(f1, np.array([14774.0]), 0.1, 0.1, 0.001, 0.5)))
+print(len(nesterovDescent(f1, np.array([14774.0]), 0.1, 0.1, 0.001)))
