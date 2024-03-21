cf2b08cdcb5c49de49ba027cccf60b4094fd29ca
Timothé <boyer.tim.travail@gmail.com>
2024-03-21 16:17:45 +0100

    Correction

--- Code/descentAlgorithme.py
+++ Code/descentAlgorithme.py
@@ -47,7 +47,7 @@ def descentVarientStep(function:function, evaluationPoint:np.array, stepDerivati
         counterIteration+=1
     return path
 
-def heavyBallDescent(function:function, evaluationPoint:np.array, stepDerivative:float, stepDescent:float, terminaisonCondition:float, stepMomentum: float =0.9)->list[tuple[np.array, np.array]]:
+def heavyBallDescent(function:function, evaluationPoint:np.array, stepDerivative:float, stepDescent:float, terminationCondition:float, stepMomentum: float =0.9, momentumTermination:float =0.5)->list[tuple[np.array, np.array]]:
     """Perform the heavy ball gradient descent on the specified function
 
     Args:
@@ -64,14 +64,14 @@ def heavyBallDescent(function:function, evaluationPoint:np.array, stepDerivative
     path=[]
     momentum=gradientFunction(function, evaluationPoint, stepDerivative)
     path.append((evaluationPoint.copy(), momentum.copy()))
-    while np.linalg.norm(gradientFunction(function, evaluationPoint, stepDerivative)) >= terminaisonCondition:
+    while np.linalg.norm(gradientFunction(function, evaluationPoint, stepDerivative)) >= terminationCondition or np.linalg.norm(momentum)>momentumTermination:
         evaluationPoint-=stepDescent*momentum
         momentum=stepMomentum*momentum+(1-stepMomentum)*gradientFunction(function, evaluationPoint, stepDerivative)
         path.append((evaluationPoint.copy(), momentum.copy()))
     return path
 
-def nesterovDescent(function: function, evaluationPoint: np.array, stepDerivative: float, stepDescent:float, terminaisonCondition:float)->list[tuple[np.array, np.array]]:
-    """Perform the heavy ball gradient descent on the specified function
+def nesterovDescent(function: function, evaluationPoint: np.array, stepDerivative: float, stepDescent:float, terminationCondition:float, momentumTermination:float =0.5)->list[tuple[np.array, np.array]]:
+    """Perform the nesterov descent gradient descent on the specified function
 
     Args:
         function (function): function from which we want to descend. The function must take an array in parameter even if it's à function with one variable.
@@ -84,19 +84,19 @@ def nesterovDescent(function: function, evaluationPoint: np.array, stepDerivativ
         list[tupple[np.array,np.array]]: List of evaluation points through which the descent has passed and their assiociate gradient
     """
     path=[]
-    lambdaPrevius=0
+    lambdaPrevious=0
     lambdaCurrent=1
     beta=1
     path.append(evaluationPoint.copy())
     path.append(evaluationPoint.copy())
-    while np.linalg.norm(gradientFunction(function, evaluationPoint, stepDerivative)) >= terminaisonCondition:
+    while np.linalg.norm(gradientFunction(function, evaluationPoint, stepDerivative)) >= terminationCondition or np.linalg.norm(beta*(path[-1]-path[-2]))>momentumTermination:
         evaluationPoint=evaluationPoint-stepDerivative*gradientFunction(function, path[-1]+beta*(path[-1]-path[-2]), stepDerivative)+beta*(path[-1]-path[-2])
         
         lambdaTemp=lambdaCurrent
         lambdaCurrent=(1+sqrt(1+4*lambdaCurrent**2))/2
-        lambdaPrevius=lambdaTemp
+        lambdaPrevious=lambdaTemp
         
-        beta=(lambdaPrevius-1)/lambdaCurrent
+        beta=(lambdaPrevious-1)/lambdaCurrent
         
         path.append(evaluationPoint)
     return path[1::]
